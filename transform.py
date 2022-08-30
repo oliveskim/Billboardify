c4d7a87ee3bc5a3015384e96466d90151e8bb1c2
BelaAckermann <isabela.ackermann@poatek.com>
2022-08-30 18:04:03 -0300

    Agora sim, tamo com o transform

--- transform.py
+++ transform.py
@@ -4,15 +4,22 @@ import pandasql as ps
 
 path_arquivo = './data/structured/billboard_10s.csv'
 
-with open(path_arquivo, 'r'):
-    df = pd.read_csv(path_arquivo)
+df = pd.read_csv(path_arquivo)
 
-#distinct_artists_df = pd.DataFrame(columns='Artist')
+distinct_artists_list = []
 
-df2 = df['Artists'].split(', ')
+for i in df['Artists']:
+    for n in range(len(i.split(', '))):
+        if i.split(', ')[n] not in distinct_artists_list:
+            distinct_artists_list.append(i.split(', ')[n])
 
-q1 = """SELECT DISTINCT(Artists) FROM df"""
+df2 = pd.DataFrame(distinct_artists_list, columns= ['Artists'], index=range(1, 101))
+
+q1 = """SELECT DISTINCT(Artists) FROM df2"""
 
 distinct_artists_df = ps.sqldf(q1, locals())
 
-print(distinct_artists_df.head(5))
+path_arquivo = './data/structured/distinct_artists_billboard_10s.csv'
+
+with open(path_arquivo, 'w'):
+    distinct_artists_df.to_csv(path_arquivo, index=False)
